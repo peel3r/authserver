e9809285bfd0fdb093091703f282b89e9e380954
pjakubowski <p@weljakubowski.com>
2016-08-11 12:08:26 +0100

    adds express setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('server listening on', port);
