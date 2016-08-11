1a60691565e63ae249c5bef44bee28a656a8f0ad
pjakubowski <p@weljakubowski.com>
2016-08-11 13:04:40 +0100

    adds router

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
+const router = require('./router')
 const app = express();
 
 //app
 
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}));
+router(app);
 
 //server
 const port = process.env.PORT || 3090;
